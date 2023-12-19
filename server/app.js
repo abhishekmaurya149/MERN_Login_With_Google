79d289f19b92751e9f404a57d689f0340815ef74
Abhishek Maurya <144514324+abhishekmaurya149@users.noreply.github.com>
2023-12-19 11:21:38 +0530

    add backend files

---
+++ server/app.js
@@ -0,0 +1,111 @@
+require("dotenv").config();
+const express = require("express");
+const app = express();
+const cors = require("cors");
+require("./db/conn")
+const PORT = 6005;
+const session = require("express-session");
+const passport = require("passport");
+const OAuth2Strategy = require("passport-google-oauth2").Strategy; //strategy is an object
+const userdb = require("./model/userSchema")
+
+const clientid = "your id"
+const clientsecret = "your secret"
+
+
+app.use(cors({
+    origin:"http://localhost:3000",
+    methods:"GET,POST,PUT,DELETE",
+    credentials:true,
+    // Headers:"Content-TypeError,X-Auth-Token,Origin,Authorization"
+}));
+
+
+
+app.use(express.json());
+
+
+// setup session
+app.use(session({
+    secret:"202dfasefs@@#sdf#",
+    resave:false,
+    saveUninitialized:true
+}))
+
+// setuppassport
+app.use(passport.initialize());
+app.use(passport.session());
+
+passport.use(
+    new OAuth2Strategy({
+        clientID:clientid,
+        clientSecret:clientsecret,
+        callbackURL:"/auth/google/callback",
+        scope:["profile","email"]
+    },
+    async(accessToken,refreshToken,profile,done)=>{
+        console.log("profile",profile)
+        try {
+            let user = await userdb.findOne({googleId:profile.id});
+
+            if(!user){
+                user = new userdb({
+                    googleId:profile.id,
+                    displayName:profile.displayName,
+                    email:profile.emails[0].value,
+                    image:profile.photos[0].value
+                });
+
+                await user.save();
+            }
+
+            return done(null,user)
+        } catch (error) {
+            return done(error,null)
+        }
+    }
+    )
+)
+
+passport.serializeUser((user,done)=>{
+    done(null,user);
+});
+
+passport.deserializeUser((user,done)=>{
+    done(null,user);
+});
+
+// initial google ouath login
+app.get("/auth/google",passport.authenticate("google",{scope:["profile","email"]}));
+
+app.get("/auth/google/callback",passport.authenticate("google",{
+    successRedirect:"http://localhost:3000/dashboard",
+    failureRedirect:"http://localhost:3000/login"
+}))
+
+//user data for front end
+app.get("/login/sucess",async(req,res)=>{
+    // console.log("req",req.user)
+
+    if(req.user){
+        res.status(200).json({message:"user Login",user:req.user})
+    }else{
+        res.status(400).json({message:"Not Authorized"})
+    }
+})
+
+app.get("/logout",(req,res,next)=>{
+    req.logout(function(err){
+        if(err){return next(err)}
+        res.redirect("http://localhost:3000");
+    })
+})
+
+
+// app.get("/",(req,res)=>{
+//     res.status(200).json("server start")
+// });
+
+app.listen(PORT, ()=>{
+    console.log(`servers start at port no ${PORT}`)
+})
